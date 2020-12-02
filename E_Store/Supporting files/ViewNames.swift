8c2d3554a69370591a6c31f432b9919479eb6da1
ZufarSuleimanov <z.suleimanov@icloud.com>
2020-12-02 10:23:56 +0600

    Created: Authorization file and supporting files

---
+++ E_Store/Supporting files/ViewNames.swift
@@ -0,0 +1,12 @@
+//
+//  ViewNames.swift
+//  E_Store
+//
+//  Created by Zufar Suleimanov on 02.12.2020.
+//
+
+import Foundation
+
+enum ViewControllers: String {
+    case Authorization = "Authorization"
+}

437f5c15bd98e052f90428c5be1bb726f69c6885
ZufarSuleimanov <z.suleimanov@icloud.com>
2020-12-27 22:34:17 +0600

    Changed: content of the processing module for working with firebase

--- E_Store/Authorization/Model/AuthHandlerModel.swift
+++ E_Store/Authorization/Model/AuthHandlerModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class AuthHandler {
+    
+    static func checkEnteredCode(code: String, verificationID: String) {
+        let credetional = PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: code)
+        Auth.auth().signIn(with: credetional) { (_, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "is empty")
+            } else {}
+        }
+    }
+    
+    static func getPhoneNumber(phoneNumber: String) {
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "is empty")
+            } else {
+                UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+            }
+        }
+    }
+    
+    static func createUser(email: String, password: String){
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "is empty")
+            } else {
+                UserDefaults.standard.set(authResult?.user.email, forKey: "authEmail")
+            }
+        }
+    }
+    
+    static func loginUsingEmail(email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "is empty")
+            } else {
+                UserDefaults.standard.set(authResult?.user.email, forKey: "authEmail")}
+        }
+    }
+    
+}
+

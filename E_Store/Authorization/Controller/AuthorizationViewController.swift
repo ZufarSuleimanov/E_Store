06063bd6e5223c757213dea579ce825a3bc777cf
ZufarSuleimanov <70098387+ZufarSuleimanov@users.noreply.github.com>
2021-01-17 21:07:54 +0600

    Authorization screen and modules (#5)
    * Modified and updated: Authorization.storyboard
    
    - added new kind of controllers.
    - changed controller from AuthorizationSignUpViewController to AuthorizationSignInForPhoneNumberViewController
    
    * Changed: content of the processing module for working with firebase
    
    * Changed: Content of extensions for controllers
    
    * Changed: interaction with modules and extensions

--- E_Store/Authorization/Controller/AuthorizationViewController.swift
+++ E_Store/Authorization/Controller/AuthorizationViewController.swift
@@ -6,25 +6,44 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class AuthorizationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-    
-    @IBAction func signInB(_ sender: UIButton) {
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        DispatchQueue.main.async {
+            if Auth.auth().currentUser?.uid != nil || Auth.auth().currentUser?.email != nil {
+                let storyboard = UIStoryboard(name: Storyboards.Authorization.rawValue, bundle: nil)
+                let testAuthView = storyboard.instantiateViewController(withIdentifier: ViewControllers.TestAuthView.rawValue) as! TestAuthViewViewController
+                testAuthView.modalPresentationStyle = .fullScreen
+                self.present(testAuthView, animated: true)
+            }
+        }
+    }
+ 
+    @IBAction func signIn(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: Storyboards.Authorization.rawValue, bundle: nil)
         let authorizationSignIn = storyboard.instantiateViewController(withIdentifier: ViewControllers.AuthorizationSignIn.rawValue)
         authorizationSignIn.modalPresentationStyle = .fullScreen
         present(authorizationSignIn, animated: true)
     }
-    @IBAction func signUpB(_ sender: UIButton) {
+    
+    @IBAction func signUp(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: Storyboards.Authorization.rawValue, bundle: nil)
         let authorizationSignUp = storyboard.instantiateViewController(withIdentifier: ViewControllers.AuthorizationSignUp.rawValue)
         authorizationSignUp.modalPresentationStyle = .fullScreen
         present(authorizationSignUp, animated: true)
     }
 
+    @IBAction func closeSegue(_ sender: UIStoryboardSegue) {
+        
+    }
+    
 }
